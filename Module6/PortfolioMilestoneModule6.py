76e3602d6b47e6e90b920046731c4e8d4834367e
shawn conner <shawn.conner@epsilon.com>
2025-04-26 12:26:28 -0600

    Add basic code before adding menu

--- Module6/PortfolioMilestoneModule6.py
+++ Module6/PortfolioMilestoneModule6.py
@@ -1,11 +1,11 @@
 from Module6.ItemToPurchase import ItemToPurchase
-
+from Module6.ShoppingCart import ShoppingCart
 
 if __name__ == '__main__':
 
     # Loop to get 2 items
     print("Enter 2 items\n")
-    items = []
+    cart= ShoppingCart("Shawn Conner", "April 26, 2025")
     for i in range(2):
         print("Item", i+1)
 
@@ -13,17 +13,10 @@ if __name__ == '__main__':
         current_price = float(input("Enter the item price: "))
         current_quantity = int(input("Enter the item quantity: "))
 
-        items.append(ItemToPurchase(current_name, current_price, current_quantity))
+        cart.add_item(ItemToPurchase(current_name, current_price, current_quantity,current_name))
         print()
 
-    # Loop thorugh items and print out the cost of each itme and then the total
-    print("TOTAL COST")
-
-    total = 0
-    for item in items:
-        cost = item.item_price*item.item_quantity
-        total+=cost
-
-        print(item.item_name, " ",  item.item_quantity, " @ $", item.item_price, " = $", cost, sep="")
-
-    print("Total: $", total, sep="")
+    print("get_num_items_in_cart: ", cart.get_num_items_in_cart())
+    print("get_cost_of_cart: ", cart.get_cost_of_cart())
+    cart.print_total()
+    cart.print_descriptions()
--- Module6/ShoppingCart.py
+++ Module6/ShoppingCart.py
@@ -29,20 +29,49 @@ class ShoppingCart:
             print("Item not found in cart. Nothing removed.")
 
     def modify_item(self, item:ItemToPurchase):
+        item_found = False
+        for i in range(len(self.cart_items)):
+            if self.cart_items[i].item_name == item.item_name:
+                item_found = True
+                if item.item_description  != "none" and item.item_quantity  != 0 and item.item_price != 0.0:
+                    self.cart_items[i].item_description = item.item_description
+                    self.cart_items[i].item_price = item.item_price
+                    self.cart_items[i].item_quantity = item.item_quantity
+                break
+
+        if not item_found:
+            print("Item not found in cart. Nothing modified.")
 
 
     def get_num_items_in_cart(self):
-        cost = self.item_price * self.item_quantity
-        print("Iem cost:",cost)
+        total_items = 0
+        for item in self.cart_items:
+            total_items += item.item_quantity
+        return total_items
 
     def get_cost_of_cart(self):
-        cost = self.item_price * self.item_quantity
-        print("Iem cost:",cost)
+        total_cost = 0
+        for item in self.cart_items:
+            total_cost += item.get_total_cost()
+        return total_cost
 
+    #Outputs total of objects in cart.
+    #If cart is empty, output this message: SHOPPING CART IS EMPTY
     def print_total(self):
-        cost = self.item_price * self.item_quantity
-        print("Iem cost:",cost)
+        if len(self.cart_items) == 0:
+            print("SHOPPING CART IS EMPTY")
+        else:
+            print(self.customer_name, "'s Shopping Cart - ", self.current_date, sep="" )
+            print("Number of Items:", self.get_num_items_in_cart())
+            for item in self.cart_items:
+                item.item_report_record()
+            print("Total: $", self.get_cost_of_cart(), sep="")
 
     def print_descriptions(self):
-        cost = self.item_price * self.item_quantity
-        print("Iem cost:",cost)
+        if len(self.cart_items) == 0:
+            print("SHOPPING CART IS EMPTY")
+        else:
+            print(self.customer_name, "'s Shopping Cart - ", self.current_date, sep="")
+            print("Item Descriptions")
+            for item in self.cart_items:
+                item.print_item_description()
